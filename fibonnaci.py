6b2fac27ae8ef064a74a30b3ef64625c38a9dda8
Rahul <rahul.doit53@gmail.com>
2020-03-31 02:03:26 +0530

    GCD and Fibonnaci

--- fibonnaci.py
+++ fibonnaci.py
@@ -2,7 +2,9 @@ def fibonacci(n):
     array = [0 for i in range(n)]
     array[0] = 0
     array[1] = 1
-    for i in range(2,n):
+    for i in range(2, n):
         array[i] = array[i-1] + array[i-2]
     return array[n-1]
+
+
 print(fibonacci(99))
