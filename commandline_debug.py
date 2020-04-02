6f6b793b27a5b6d189ce5ea23a4fb28b8f7583f1
Rahul <rahul.doit53@gmail.com>
2020-04-02 18:54:02 +0530

    Tested debugger on module

--- commandline_debug.py
+++ commandline_debug.py
@@ -5,5 +5,7 @@ Module to analyze the flow of the debugger in pyton
 
 import pdb
 filename = __file__
+# Note : __file__ returns the file name of the file calling the function
+
 pdb.set_trace()
 print(f'The file name is {filename}')
