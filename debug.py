da0d4e0996d6b843b5e67d5b1152af2ad12f1bfd
Rahul <rahul.doit53@gmail.com>
2020-04-02 20:26:46 +0530

    Corrected function get_path() to return value

--- debug.py
+++ debug.py
@@ -3,6 +3,9 @@ Implementing Python debugger pdb for efficient debugging of code and analyzing s
 '''
 
 
+import os
+
+
 def test_function():
     ''' Function with miscellaneous functionality to test the debugging techniques in python '''
     var = int(input('Enter a value: '))
@@ -18,14 +21,23 @@ def test_function_2():
     print(var)
 
 
+def get_path(filename):
+    ''' Returns file path or no string if no path '''
+    head, tail = os.path.split(filename)
+    import pdb
+    pdb.set_trace()
+    return head
+
+
 def get_file_name():
     ''' Using file name argument and analyzing flow of the debugger '''
     filename = __file__
     import pdb
     pdb.set_trace()
-    print(f'path = {filename}')
+    print(f'path = {get_path(filename)}')
 
 
 if __name__ == '__main__':
     # test_function()
-    test_function_2()
+   # test_function_2()
+    get_file_name()
